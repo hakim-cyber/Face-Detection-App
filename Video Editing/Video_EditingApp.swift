c44c6143f4ef0e9a39c5439622310b0c010a0c85
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-07-03 14:38:55 +0400

    Initial Commit

---
+++ Video Editing/Video_EditingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_EditingApp.swift
+//  Video Editing
+//
+//  Created by aplle on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Video_EditingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
